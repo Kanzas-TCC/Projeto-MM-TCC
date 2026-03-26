94c79b60df2a139786759955e21c90ecabfb1c7a
Felipe Silva <faugusto11@outlook.com>
2026-03-26 09:49:00 -0300

    Adição das rotas de autenticação no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,31 +1,32 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import auth-router from "./routes/auth-route.js"
 
 const app = express();
 
 // Fala que é pra renderizar em ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const publicPath = path.join(__dirname, '../../frontend/src');
+const publicPath = path.join(__dirname, "../../frontend/src");
 
-// Define ONDE os arquivos de frontend tão e por onde o res render vai considerar como raíz
-// app.set('views', '/home/user1/projeto-tcc-dev/frontend');
-app.set('views', publicPath);
+// Define ONDE os arquivos de frontend estão e por onde o res render vai considerar como raíz
+// app.set("views", "/home/user1/projeto-tcc-dev/frontend");
+app.set("views", publicPath);
 
 // Serve arquivos estáticos (CSS, JS, imagens) da pasta frontend
-// app.use(express.static('/home/user1/projeto-tcc-dev/frontend')); // /var/www/modamym-dev
+// app.use(express.static("/home/user1/projeto-tcc-dev/frontend")); // /var/www/modamym-dev
 app.use(express.static(publicPath));
 
 /* _.-=-._.-=-._.-=-._ROTAS DE BACKEND.-=-._.-=-._.-=-._ */
 
-/* Nada por enquanto, mas todas as rotas de backend devem ter o prefixo '/api', como no exemplo abaixo */
+/* Nada por enquanto, mas todas as rotas de backend devem ter o prefixo "/api", como no exemplo abaixo */
 
-// app.get('/api/rota', (req, res) => {
-//     res.send('Hello World!'); -----> isso manda o texto 'Hello World!' pra quem fez a requisição pra essa rota.
+// app.get("/api/rota", (req, res) => {
+//     res.send("Hello World!"); -----> isso manda o texto "Hello World!" pra quem fez a requisição pra essa rota.
 // });
 
 /* _.-=-._.-=-._.-=-._ROTAS DE FRONTEND.-=-._.-=-._.-=-._ */
@@ -33,42 +34,36 @@ app.use(express.static(publicPath));
 //Isso daqui é uma rota que renderiza o index, então ao abrir dev.modamym.com.br você vai ver primeiro a página que o res.render carregar
 /* ========== INDEX =========*/
 // o REQ é tudo que o frontend mandou pro backend, já o RES é tudo que o backend vai mandar pro frontend
-app.get('/', (req, res) => {
-    // O Express vai procurar por 'index.ejs' dentro de '/var/www/modamym-dev'
+app.get("/", (req, res) => {
+    // O Express vai procurar por "index.ejs" dentro de "/var/www/modamym-dev"
 
-    //res.render('index');  //NÃO PRECISA DA EXTENSÃO .ejs, o 'index' no começo do render aponta pra view 'var/www/modamym-dev/index.ejs'
+    //res.render("index");  //NÃO PRECISA DA EXTENSÃO .ejs, o "index" no começo do render aponta pra view "var/www/modamym-dev/index.ejs"
 
     /* ====== se alguma rota precisar passar alguma variavel pro frontend é só colocar objeto na frente ======*/
 
-    res.render('pages/auth/login.ejs');
+    res.render("pages/auth/login.ejs");
 });
 
 /* ========== Autenticação ==========*/
 
-app.get('/sign-up', (req, res) => {
-    res.render('pages/auth/sign-up.ejs');
-});
-
-app.get('/forgot-password', (req, res) => {
-    res.render('pages/auth/forgot-password.ejs');
-});
+app.use("/auth", router);
 
 
 /* ========== Dados financeiros ==========*/
 
-app.get('/dashboard', (req, res) => {
-    res.render('pages/finances/dashboard.ejs');
+app.get("/dashboard", (req, res) => {
+    res.render("pages/finances/dashboard.ejs");
 });
 
-app.get('/accounting', (req, res) => {
-    res.render('pages/finances/accounting.ejs');
+app.get("/accounting", (req, res) => {
+    res.render("pages/finances/accounting.ejs");
 })
 
 /* ========== Mercado Livre ==========*/
 
-app.get('/ads-ml', (req, res) => {
-    res.render('pages/mercado-livre/ads-ml.ejs');
+app.get("/ads-ml", (req, res) => {
+    res.render("pages/mercado-livre/ads-ml.ejs");
 })
 
 // Roda o servidor inteiro do backend na porta 3000 do servidor
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
